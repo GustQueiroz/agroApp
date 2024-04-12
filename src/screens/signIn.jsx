2048ed46f7696552dd1f11ee668cbe8ffae3020b
contato.gustavoqlima <contato.gustavoqlima@gmail.com>
2024-04-11 21:23:22 -0300

    signin estrutura

--- src/screens/signIn.jsx
+++ src/screens/signIn.jsx
@@ -1,7 +1,22 @@
 const SignIn = () => {
   return (
     <>
-      <div>aaaa</div>
+      <div>
+        <div>
+          <div>
+            <label className="form-label">Usuario</label>
+            <input type="text" className="form-control" id="user"></input>
+          </div>
+          <div>
+            <label className="form-label">Senha</label>
+            <input type="text" className="form-control" id="password"></input>
+          </div>
+          <div>
+            <label className="">Esqueceu sua Senha?</label>
+          </div>
+        </div>
+        <div></div>
+      </div>
     </>
   );
 };
